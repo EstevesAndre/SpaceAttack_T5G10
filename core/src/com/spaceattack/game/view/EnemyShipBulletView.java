5bf070e2d7f7b563a6efec1520d68b02e3e9af9c
estevesandre <up201606673@fe.up.pt>
2018-05-17 10:37:32 +0100

    created user and enemy ship View

--- core/src/com/spaceattack/game/view/EnemyShipBulletView.java
+++ core/src/com/spaceattack/game/view/EnemyShipBulletView.java
@@ -1,8 +1,49 @@
 package com.spaceattack.game.view;
 
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.spaceattack.game.SpaceAttackGame;
+
 /**
  * Created by estev on 17/05/2018.
  */
 
-public class EnemyShipBulletView {
+public class EnemyShipBulletView extends ObjectView{
+
+    /**
+     * Constructs a enemy ship bullet view.
+     *
+     * @param game the game this view belongs to. Needed to access the
+     *             asset manager to get textures.
+     */
+    public EnemyShipBulletView(SpaceAttackGame game) {
+        super(game);
+    }
+
+    /**
+     * Creates a sprite representing this enemy ship bullet.
+     *
+     * @param game the game this view belongs to. Needed to access the
+     *             asset manager to get textures.
+     * @return the sprite representing the enemy ship bullet
+     */
+    @Override
+    public Sprite createSprite(SpaceAttackGame game) {
+
+        return new Sprite(createEnemyShipBulletRegion(game));
+    }
+
+    /**
+     * Creates the texture used for enemy ship bullet
+     *
+     * @param game the game this view belongs to. Needed to access the
+     *             asset manager to get textures.
+     * @return the texture used for each bullet
+     */
+    private TextureRegion createEnemyShipBulletRegion(SpaceAttackGame game) {
+        Texture EnemyShipBulletTexture = game.getAssetManager().get("bullet_Enemy_Ship.png");
+        return new TextureRegion(EnemyShipBulletTexture, EnemyShipBulletTexture.getWidth(), EnemyShipBulletTexture.getHeight());
+    }
+
 }
--- core/src/com/spaceattack/game/view/UserBulletView.java
+++ core/src/com/spaceattack/game/view/UserBulletView.java
@@ -1,8 +1,49 @@
 package com.spaceattack.game.view;
 
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.spaceattack.game.SpaceAttackGame;
+
 /**
  * Created by estev on 17/05/2018.
  */
 
-public class UserBulletView {
+public class UserBulletView extends ObjectView{
+
+    /**
+     * Constructs a user ship bullet view.
+     *
+     * @param game the game this view belongs to. Needed to access the
+     *             asset manager to get textures.
+     */
+    public UserBulletView(SpaceAttackGame game) {
+        super(game);
+    }
+
+    /**
+     * Creates a sprite representing this bullet user ship.
+     *
+     * @param game the game this view belongs to. Needed to access the
+     *             asset manager to get textures.
+     * @return the sprite representing the user ship bullet
+     */
+    @Override
+    public Sprite createSprite(SpaceAttackGame game) {
+
+        return new Sprite(createUserBulletRegion(game));
+    }
+
+    /**
+     * Creates the texture used for user ship bullet
+     *
+     * @param game the game this view belongs to. Needed to access the
+     *             asset manager to get textures.
+     * @return the texture used for each bullet
+     */
+    private TextureRegion createUserBulletRegion(SpaceAttackGame game) {
+        Texture UserShipBulletTexture = game.getAssetManager().get("bullet_User_Ship.png");
+        return new TextureRegion(UserShipBulletTexture, UserShipBulletTexture.getWidth(), UserShipBulletTexture.getHeight());
+    }
+
 }
