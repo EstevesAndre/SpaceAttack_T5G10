b0db4bd239ecedc33de574e00e517a06301d0bd0
estevesandre <up201606673@fe.up.pt>
2018-05-25 02:35:10 +0100

    created pause menu

---
+++ core/src/com/spaceattack/game/view/screens/PauseMenu.java
@@ -0,0 +1,30 @@
+package com.spaceattack.game.view.screens;
+
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.spaceattack.game.SpaceAttackGame;
+
+/**
+ * Created by estev on 24/05/2018.
+ */
+
+public class PauseMenu extends OptionsMenu {
+
+    PauseMenu(Viewport viewport, SpaceAttackGame game) {
+        super(viewport, game);
+        setMessage();
+    }
+
+    @Override
+    protected void createStageObjects() {
+        initStage();
+        addResumeButton();
+        addBackToMainMenuButton();
+        addExitButton();
+        this.addActor(table);
+    }
+
+    @Override
+    protected void setMessage() {
+        message.setText("GAME PAUSED");
+    }
+}
