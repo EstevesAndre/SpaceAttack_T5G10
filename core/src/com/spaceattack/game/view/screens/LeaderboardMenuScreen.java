e9ff75ddc646c606709ac33fa244729252577ca4
estevesandre <up201606673@fe.up.pt>
2018-05-25 02:34:05 +0100

    created leaderboard menu

---
+++ core/src/com/spaceattack/game/view/screens/LeaderboardMenuScreen.java
@@ -0,0 +1,18 @@
+package com.spaceattack.game.view.screens;
+
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.spaceattack.game.SpaceAttackGame;
+
+/**
+ * Created by estev on 25/05/2018.
+ */
+
+public class LeaderboardMenuScreen extends MenuScreen {
+
+    LeaderboardMenuScreen(SpaceAttackGame game) {
+        super(game);
+    }
+
+}
