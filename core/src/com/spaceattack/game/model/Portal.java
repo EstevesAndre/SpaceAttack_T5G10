60e0d50e90cee33a14aa5568c8b029204a9703bc
estevesandre <up201606673@fe.up.pt>
2018-05-28 15:16:50 +0100

    completed model package documentation

--- core/src/com/spaceattack/game/model/Portal.java
+++ core/src/com/spaceattack/game/model/Portal.java
@@ -1,11 +1,9 @@
 package com.spaceattack.game.model;
 
+/**
+ * Portal representing the place where Enemy Ship spawn
+ */
 public class Portal extends GameObject{
-
-    /**
-     * If this portal has spawned a unit this step
-     */
-    private boolean exhausted;
 
     /**
      * Constructor of a portal.
@@ -17,26 +15,5 @@ public class Portal extends GameObject{
     public Portal(float x, float y, float rotation)
     {
         super(x, y, rotation);
-        this.exhausted = false;
-    }
-
-    /**
-     * Checks if the portal has spawn a unit this step
-     *
-     * @return the exhausted field
-     */
-    public boolean isExhausted()
-    {
-        return exhausted;
-    }
-
-    /**
-     * Set the exhausted status
-     *
-     * @param exhausted the status to be attributed to exhausted
-     */
-    public void setExhausted(boolean exhausted)
-    {
-        this.exhausted = exhausted;
     }
 }
